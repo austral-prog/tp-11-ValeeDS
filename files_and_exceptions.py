409ba18a4dc53e4879c95a9079862272f6ec3efd
Valeria Del Savio <valeria.delsavio@hotmail.com>
2025-06-21 21:02:41 -0300

    Update files_and_exceptions.py

--- files_and_exceptions.py
+++ files_and_exceptions.py
@@ -5,7 +5,29 @@ def read_file_to_dict(filename):
     :return: dict - diccionario con listas de montos por producto.
     :raises: FileNotFoundError - si el archivo no existe.
     """
-    return {}
+    import csv
+
+    csv_data = []
+
+    try:
+        with open(filename, "r") as f:
+            csv_reader = csv.reader(f, delimiter=';')
+            for row in csv_reader:
+                csv_data = row
+
+    except FileNotFoundError:
+        raise FileNotFoundError("Error: The file does not exist.")
+
+    ventas = dict()
+
+    for reg in csv_data:
+        info = ( reg[ : reg.find(":") ] , reg[ reg.find(":")+1 : ] )
+        if info[0] in ventas.keys() and info[0] != '':
+            ventas[info[0]].append(float(info[1]))
+        elif info[0] != '':
+            ventas[info[0]] = [float(info[1])]
+
+    return ventas
 
 
 def process_dict(data):
@@ -14,4 +36,8 @@ def process_dict(data):
     :param data: dict - diccionario a procesar.
     :return: None
     """
-    pass
+    for prod, ventas in data.items():
+        tot = sum(ventas)
+        prom = tot / len(ventas)
+
+        print(f"{prod}: ventas totales ${tot:.2f}, promedio ${prom:.2f}")
